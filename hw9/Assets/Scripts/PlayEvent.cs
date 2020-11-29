eb4a4d6fda893b787a562e1dec6d780014604ab0
l563668908 <563668908@qq.com>
2020-11-29 19:10:27 +0800

    Initial commit

---
+++ hw9/Assets/Scripts/PlayEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEvent : MonoBehaviour
+{
+    public string eventName;
+
+    public void Click()
+    {
+        AkSoundEngine.PostEvent(eventName, this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
